4fd52e2f31908757b69872fe79c72b9ab95d5c92
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-05-01 12:18:44 +0400

    newNumber

---
+++ lastToFirst.js
@@ -0,0 +1,9 @@
+let numbers = "125";
+let lastNumber = numbers[numbers.length - 1];
+let firstNumber = numbers[0];
+let middleNumber = numbers.slice(1, numbers.length - 1);
+let newNumber = lastNumber + middleNumber + firstNumber;
+console.log(newNumber);
+// console.log(middleNumber);
+// console.log(firstNumber);
+// console.log(lastNumber);
