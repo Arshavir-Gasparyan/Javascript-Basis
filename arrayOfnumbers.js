8c2b92e9eb36e0b93e56851d78c1731937544d26
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-05-02 18:34:10 +0400

    halfDone

---
+++ arrayOfnumbers.js
@@ -0,0 +1,9 @@
+let arr = [21, -9, 34, -3, -71, 35];
+let number = 34;
+let a = [];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === number) {
+    console.log(arr.indexOf(arr[i]));
+  } else {
+  }
+}
